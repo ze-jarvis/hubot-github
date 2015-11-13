d298f569c2fb1834a99ed0f54c02c1394ab6cd44
linsolas <linsolas@gmail.com>
2015-11-13 13:16:42 +0100

    test: new test, with promise and stuff!

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
 'use strict';
 
 var gh = require('github');
+var promisify = require('promisify-node');
 
 module.exports = function(robot) {
 
+  //var repoUrl = 'https://github.com/ze-jarvis/ze-hubot-github';
+
   var github = new gh({
-    version: '3.0.0'
+    version: '3.0.0',
+    debug: false
   });
 
   robot.hear('pr', function(res) {
@@ -13,8 +17,35 @@ module.exports = function(robot) {
   });
 
   robot.hear('list-pr', function(res) {
-    console.log('Called > ', res);
-    github.issues.getAll()
+    var wrap = promisify(github.issues.repoIssues);
+
+    res.$promise = wrap({
+      user: 'ze-jarvis',
+      repo: 'ze-hubot-github'
+    }).then(function(issues) {
+      //console.log(JSON.stringify(res));
+      for (var i = 0; i < issues.length; i++) {
+        //console.log('issue #' + res[i].number);
+        res.reply('issue #' + issues[i].number);
+      }
+    });
   });
+
+  var listIssues = function(robot) {
+    console.log('list issues');
+    github.issues.repoIssues({
+      user: 'ze-jarvis',
+      repo: 'ze-hubot-github'
+    }, function(err, res) {
+      if (err) {
+        console.log('ERROR', err);
+      }
+      //console.log(JSON.stringify(res));
+      for (var i = 0; i < res.length; i++) {
+        //console.log('issue', res[i]);
+        robot.reply('issue #' + res.number);
+      }
+    });
+  }
 
 };
--- test/test-github.spec.js
+++ test/test-github.spec.js
@@ -2,11 +2,13 @@
 
 var chai = require('chai');
 var sinonChai = require('sinon-chai');
+var chaiPromise = require('chai-as-promised');
 chai.use(sinonChai);
 var expect = chai.expect;
 
 var koala = require('../index.js');
 var robotMock = require('./mocks/robot.mock.js');
+
 
 describe('Test Github access', function() {
 
@@ -20,8 +22,13 @@ describe('Test Github access', function() {
   it('test PR', function() {
     var response = panda.receive('pr');
     // expect res reply with 'I hear you'
-    //expect(response.reply.calledWith('I hear you!')).to.be.true;
     expect(response.reply).to.have.been.calledWith('I hear you!');
+  });
+
+  it('should list all the issues', function() {
+    var response = panda.receive('list-pr').$promise.then(function() {
+      expect(response.reply).to.have.been.calledWith('issue #1');
+    });
   });
 
 });
